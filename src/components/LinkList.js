6dc4c5c29353068112d340598835790250337913
Safarali <mr.safarali@gmail.com>
2019-04-23 00:19:30 -0600

    Subscribe to new links and votes

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,6 +3,26 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Link from './Link';
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                    id
+                }
+            }
+        }
+    }
+`;
 
 export const FEED_QUERY = gql `
     {
@@ -26,6 +46,34 @@ export const FEED_QUERY = gql `
         }
     }
 `;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 const LinkList = () => {
 
     const updateCacheAfterVote = (store, createdVote, linkId) => {
@@ -34,14 +82,43 @@ const LinkList = () => {
         const votedLink = data.feed.links.find(link => link.id === linkId);
         votedLink.votes = createdVote.link.votes;
         store.writeQuery({ query: FEED_QUERY, data })
-    }
+    };
+
+    const subscribeToNewLinks = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_LINKS_SUBSCRIPTION,
+            updateQuery: (prev, { subscriptionData }) => {
+                if (!subscriptionData.data) return prev;
+                const { newLink } = subscriptionData.data;
+                const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+                if (exists) return prev;
+
+                return Object.assign({}, prev, {
+                    feed: {
+                        links: [newLink, ...prev.feed.links],
+                        count: prev.feed.links.length + 1,
+                        __typename: prev.feed.__typename
+                    }
+                })
+            }
+        })
+    };
+
+    const subscribeToNewVotes = subscribeToMore => {
+        subscribeToMore({
+            document: NEW_VOTES_SUBSCRIPTION
+        })
+    };
 
     return (
         <Query query={FEED_QUERY}>
-            {({ loading, error, data }) => {
-               if (loading) return <div>Fetching</div>
-               if (error) return <div>Error</div>
+            {({ loading, error, data, subscribeToMore }) => {
+                if (loading) return <div>Fetching</div>
+                if (error) return <div>Error</div>
 
+                subscribeToNewLinks(subscribeToMore);
+                subscribeToNewVotes(subscribeToMore);
+               
                 const linksToRender = data.feed.links;
                 return (
                     <div>
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 import { BrowserRouter } from 'react-router-dom';
-import App from './components/App';
-import { AUTH_TOKEN } from './constants';
 import './styles/index.css'
-
 import * as serviceWorker from './serviceWorker'
-
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import App from './components/App';
+import { AUTH_TOKEN } from './constants';
 
-
+// Http
 const httpLink = createHttpLink({
     uri: 'http://localhost:4000'
 });
@@ -28,10 +29,36 @@ const authLink = setContext((_, { headers }) => {
   }
 });
 
-const client = new ApolloClient({
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache()
+// Sockets
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
 });
+
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(httpLink)
+);
+
+
+// Client
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+
 
 const jsx = (
   <BrowserRouter>
